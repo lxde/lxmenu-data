efd24d6c93ffb1ae75636ecd84a72297b1272908
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2014-05-13 02:21:17 +0800

    Fix autogen.sh and rename configure.in to configure.ac

--- autogen.sh
+++ autogen.sh
@@ -10,22 +10,26 @@ cd "$SRC_DIR"
 AUTOMAKE=${AUTOMAKE:-automake}
 AM_INSTALLED_VERSION=$($AUTOMAKE --version | sed -e '2,$ d' -e 's/.* \([0-9]*\.[0-9]*\).*/\1/')
 
-if [ $AM_INSTALLED_VERSION < 1.10 ];then
+# FIXME: we need a better way for version check later.
+case "$AM_INSTALLED_VERSION" in
+    1.1[0-9])
+	;;
+    *)
 	echo
-	echo "You must have automake > 1.10 installed to compile lxmenu-data."
+	echo "You must have automake 1.10 or newer installed."
 	echo "Install the appropriate package for your distribution,"
 	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
 	exit 1
-fi
-
-set -x
+	;;
+esac
 
 if [ "x${ACLOCAL_DIR}" != "x" ]; then
   ACLOCAL_ARG=-I ${ACLOCAL_DIR}
 fi
 
+set -x
+
 ${ACLOCAL:-aclocal$AM_VERSION} ${ACLOCAL_ARG}
-AUTOMAKE=$AUTOMAKE libtoolize -c --automake --force
 AUTOMAKE=$AUTOMAKE intltoolize -c --automake --force
 $AUTOMAKE --add-missing --copy --include-deps
 ${AUTOCONF:-autoconf$AC_VERSION}
@@ -33,7 +37,7 @@ ${AUTOCONF:-autoconf$AC_VERSION}
 rm -rf autom4te.cache
 
 if test "x$NOCONFIGURE" = "x"; then
-	./configure $@
+    ./configure $@
 fi
 
 cd "$OLD_DIR"
